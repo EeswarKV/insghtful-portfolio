00bda32e894755ff25d80a1edb46643e3be9ef90
Venkata Aparna Oruganti <venkata.aparna.oruganti@ing.com>
2023-08-18 17:24:57 +0200

    Authentication implementation for login logout

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,6 @@
+from urllib.parse import quote_plus, urlencode
 from flask import Flask, render_template, redirect, url_for, session
+import flask
 from src.models import User, db
 from src.dashboard import get_portfolio_data
 from src.modify_entry import edit_portfolio_entry, delete_portfolio_entry
@@ -7,15 +9,41 @@ from src.login_register import get_user_register, get_user_login, bcrypt
 from src.home import get_home_data
 from functools import wraps
 from src.forms import LoginForm, RegistrationForm
+from dotenv import find_dotenv, load_dotenv
+from authlib.integrations.flask_client import OAuth
+from os import environ as env
+from authlib.integrations.flask_oauth2 import ResourceProtector
+import ssl 
+ssl._create_default_https_context = ssl._create_unverified_context
 
 app = Flask(__name__, static_url_path='/static')
 app.config.from_object('config.Config')
 db.init_app(app) 
 bcrypt.init_app(app)
 
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+app.secret_key = env.get("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    audidance=env.get("AUTH0_AUDIANCE"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
 # Create the tables
 with app.app_context():
     db.create_all()
+
 
 #decorator to check if user logged in and returns user if successful
 def login_required(f):
@@ -28,20 +56,37 @@ def login_required(f):
         return f(user, *args, **kwargs)
     return decorated_function
 
+#decorator to check if user logged in and returns user if successful
+def login_required_for_new_way(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        token = oauth.auth0.authorize_access_token()
+        values = token['userinfo']
+        print(values)
+        emailValue = values['name'].strip()
+        user = User.query.filter_by(email=emailValue).first() if emailValue else None
+        print(user)
+        if user is None:
+            print("no user info present")
+            return redirect(url_for('login'))
+        return f(user, *args, **kwargs)
+    return decorated_function
+
 #login route returns to login template
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     try:
-        form = LoginForm()
-        if form.validate_on_submit():
-            return get_user_login(form)
-        return render_template('login.html', form=form)
+        return oauth.auth0.authorize_redirect(redirect_uri=url_for("home", _external=True))
+        # form = LoginForm()
+        # if form.validate_on_submit():
+        #     return get_user_login(form)
+        # return render_template('login.html', form=form)
     except Exception as e:
         print(f"Error trying to login: {e}")
         return None
 
-#returns to log template once registration successful
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     try:
@@ -64,8 +109,8 @@ def invest(user):
         return None
     
 #returns to home template
-@app.route('/home', methods=['GET', 'POST'])
-@login_required
+@app.route('/home', methods=['GET'])
+@login_required_for_new_way
 def home(user):
     try:
         return get_home_data(user)
@@ -107,6 +152,19 @@ def handle_entry_action(user, action_function, entry_id):
 @app.route('/logout')
 def logout():
     try:
+        session.clear()
+        return redirect(
+        "https://"
+        + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("login", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
         session.pop('user_id', None)  # Remove the user ID from the session
         return redirect(url_for('login')) 
     except Exception as e:
